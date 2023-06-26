df8d8adfc19f7c7103eea8cb669bb445c2f3086d
Shri10 <pethe.shriraj@gmail.com>
2023-06-26 22:00:11 +0530

    [Shri10] Created UC4

--- DS_LinkedLists/Program.cs
+++ DS_LinkedLists/Program.cs
@@ -10,13 +10,13 @@ namespace DS_LinkedLists
 
             LinkedList LList = new LinkedList();
 
-            /*LList.AddNode(56);
+            LList.AddNode(56);
             LList.AddNode(30);
-            LList.AddNode(70);*/
+            LList.AddNode(70);
 
-            LList.AddToStart(70);
+            /*LList.AddToStart(70);
             LList.AddToStart(30);
-            LList.AddToStart(56);
+            LList.AddToStart(56);*/
 
             LList.PrintList();
         }
