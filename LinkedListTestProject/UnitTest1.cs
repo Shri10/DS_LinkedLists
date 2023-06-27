cfd36e10aa24463f8838f028a18af9b45b8b4cec
Shri10 <pethe.shriraj@gmail.com>
2023-06-27 10:17:24 +0530

    [Shri10] Created UC9

--- LinkedListTestProject/UnitTest1.cs
+++ LinkedListTestProject/UnitTest1.cs
@@ -31,6 +31,20 @@ namespace LinkedListTestProject
             Assert.AreEqual(40, resultAfter30.Data);
         }
 
+        [TestMethod]
+        public void WhenValueDeletedSizeDecrease()
+        {
+            LinkedList LList = new LinkedList();
+            LList.AddNode(56);
+            LList.AddNode(30);
+            LList.AddNode(70);
+            LList.InsertAfter(30, 40);
+            LList.DeleteWithValue(40);
+
+            int size = LList.Size();
+            Assert.AreEqual(3, size);
+        }
+
 
     }
 }
